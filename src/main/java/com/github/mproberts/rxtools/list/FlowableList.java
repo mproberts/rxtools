974d459a9b019572eb8493b5b7db4e833e240248
remvst <remvst@gmail.com>
2019-01-18 12:32:23 -0500

    Dummy commit

--- src/main/java/com/github/mproberts/rxtools/list/FlowableList.java
+++ src/main/java/com/github/mproberts/rxtools/list/FlowableList.java
@@ -110,6 +110,7 @@ public abstract class FlowableList<T>
         return new SwitchMapFlowableList<>(list);
     }
 
+    
     /**
      * Transforms an FlowableList containing VisibilityState items into an FlowableList
      * which includes in its emissions the changes in visibility status of the items within the
